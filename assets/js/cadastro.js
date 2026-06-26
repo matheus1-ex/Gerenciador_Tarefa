b6580d9bd905bc07a44e3d5749b91c5fb0cf6418
Matheus Almeida <juliomatheusalmeida90@gmail.com>
2026-06-26 18:01:30 -0300

    Adicionei um javascript na tela de cadastro

---
+++ assets/js/cadastro.js
@@ -0,0 +1,7 @@
+function Cadastrar() {
+    let nome = document.getElementyById("nome").value;
+    let email = document.getElementyById("email").value;
+    let senha = document.getElementyById("senha").value;
+    let confirmeSenha = document.getElementyById("confirmeSenha").value;
+    let termos = document.getElementyById("termos").checked;
+}
